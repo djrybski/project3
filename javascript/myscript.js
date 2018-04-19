238ff430518aafd0d3a7951a00a7804e27f18c41
djrybski <31292123+djrybski@users.noreply.github.com>
2018-04-18 19:49:43 -0500

    Add files via upload

---
+++ javascript/myscript.js
@@ -0,0 +1,14 @@
+
+ function initMap() {
+  var fishinglocation = {lat: 41.820579 , lng: -87.821949};
+  var map = new google.maps.Map(document.getElementById(“map”), { 
+    zoom: 4, 
+    center: location
+});
+    marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
+}
+</script>
+<script async defer src=“”>https://maps.googleapis.com/maps/api/js?key=AIzaSyBRPLVq-1vIhtQtSXY2rac8322VmXK8J6Q&callback=initMap”></script>
