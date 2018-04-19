9ba6f190a815c5f1f8e331c43da463511d51f1e8
djrybski <31292123+djrybski@users.noreply.github.com>
2018-04-18 21:33:20 -0500

    Update myscript.js

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,5 +1,4 @@
-
- function initMap() {
+  function initMap() {
   var fishinglocation = {lat: 41.820579 , lng: -87.821949};
   var map = new google.maps.Map(document.getElementById(“map”), { 
     zoom: 4, 
