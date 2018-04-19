4cc800c17e684db10989c55ae89ca0e773708481
djrybski <31292123+djrybski@users.noreply.github.com>
2018-04-18 22:50:24 -0500

    Update myscript.js

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,6 +1,6 @@
   function initMap() {
   var location = {lat: 41.820579 , lng: -87.821949};
-  var map = new google.maps.Map(document.getElementById(“map”), { 
+  var map = new google.maps.Map(document.getElementById('map'), { 
     zoom: 4, 
     center: location
 });
